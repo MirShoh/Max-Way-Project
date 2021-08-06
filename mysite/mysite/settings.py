578a07f10f8de794b4eedf9613cc27203f5f7cd9
Shohjahon Mirakov <65676018+MirShoh@users.noreply.github.com>
2021-08-06 22:03:30 +0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'maxway_db',
-        'USER': 'maxway_user',
-        'PASSWORD': 'maxway_root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
